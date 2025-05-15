15f72472ccbb37afd1048fe7ab65de0431c13664
Arthur Estigarribia <211487519+arthur-estigarribia-2000@users.noreply.github.com>
2025-05-15 00:03:01 -0300

    Create exemplo002.py
    Este exemplo aborda a noção de estruturas de decisão em Python 3.

---
+++ exemplo002.py
@@ -0,0 +1,17 @@
+# Decisão
+# Exemplo: maioridade penal no Brasil
+
+# O tipo de dados int trabalha com inteiros positivos ou negativos
+idade = int(input('Digite a sua idade em anos: '))
+
+# A estrutura if-elif-else analisa condições lógicas
+# Dentro de estruturas, a indentação (em espaços ou tabs) é obrigatória e cumulativa
+# Operadores lógicox aceitos em Python: == (igual), != (diferente), < (menor), > (maior), <= (menor ou igual) e >= (maior ou igual)
+if idade < 0:
+	# Este bloco é executado quando a condição acima for verdadeira (valor booleano True); depois dela, as demais condições da estrutura são ignoradas
+	print('Idade inválida. ')
+elif idade < 18:
+	print('Menor de idade. ')
+else:
+	# Este bloco e executado quando todas as condições forem falsas (valor booleano False)
+	print('Maior de idade. ')
