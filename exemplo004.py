bfc45f204c9034d55875b0c67e8323a557e048ef
Arthur Estigarribia <211487519+arthur-estigarribia-2000@users.noreply.github.com>
2025-05-15 00:26:41 -0300

    Create exemplo004.py
    Este exemplo aborda a noção de funções em Python 3.

---
+++ exemplo004.py
@@ -0,0 +1,30 @@
+# Funções
+# Exemplo: média de notas escolares
+
+# Estrutura def
+# Permite a reutilização de código que seria repetido muitas vezes
+# Pode ter vários parâmetros, mas apenas um retorno
+# Indentação obrigatória e cumulativa
+def media(nota1, nota2, nota3):
+	soma = float(nota1) + float(nota2) + float(nota3)
+	razao = float(soma) / 3
+	
+	# O método round(numero, casas) é recomendado para evitar erros de máquina numa operação decimal
+	# Pode ter de 0 a 15 casas decimais
+	return round(media, 1)
+
+# Entrada de dados
+n1 = float(input('Digite a nota 1: '))
+n2 = float(input('Digite a nota 2: '))
+n3 = float(input('Digite a nota 3: '))
+
+# Chamada do método já declarado (seu retorno será armazenado na variável m)
+m = media(n1, n2, n3)
+
+# Saída de dados
+print('Média: ' + str(m))
+
+if m >= 7.0:
+	print('Aprovado. ')
+else:
+	print('Reprovado. ')
