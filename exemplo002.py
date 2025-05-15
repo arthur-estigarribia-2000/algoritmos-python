4138e0db178da36c08a92215ca287c29fa39a2c6
Arthur Estigarribia <211487519+arthur-estigarribia-2000@users.noreply.github.com>
2025-05-15 00:32:12 -0300

    Update exemplo002.py

--- exemplo002.py
+++ exemplo002.py
@@ -2,15 +2,16 @@
 # Exemplo: maioridade penal no Brasil
 
 # O tipo de dados int trabalha com inteiros positivos ou negativos
-idade = int(input('Digite a sua idade em anos: '))
+idade = int(input('Digite a idade em anos: '))
 
 # Estrutura de decisão if-elif-else
 # Dentro de estruturas, a indentação (em espaços ou tabs) é obrigatória e cumulativa
-# Operadores lógicox aceitos no Python 3: == (igual), != (diferente), < (menor), > (maior), <= (menor ou igual) e >= (maior ou igual)
+# Operadores lógicos aceitos no Python 3: == (igual), != (diferente), < (menor), > (maior), <= (menor ou igual) e >= (maior ou igual)
 if idade < 0:
 	# Este bloco é executado quando a condição acima for verdadeira (valor booleano True) e as demais condições da estrutura são ignoradas
 	print('Idade inválida. ')
 elif idade < 18:
+	# Se o anterior for falso, mas este for verdadeiro, executa este bloco e ignora os posteriores
 	print('Menor de idade. ')
 else:
 	# Este bloco e executado quando todas as condições forem falsas (valor booleano False)
