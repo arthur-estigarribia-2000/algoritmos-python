a1b39b225d8bd0b96a8210746c304d7860648719
Arthur Estigarribia <211487519+arthur-estigarribia-2000@users.noreply.github.com>
2025-05-15 00:30:28 -0300

    Update exemplo001.py

--- exemplo001.py
+++ exemplo001.py
@@ -6,17 +6,21 @@
 valor = float(input('Digite o valor total ou inicial: '))
 porcentagem = float(input('Digite o percentual: '))
 
-# Operadores numericos aceitos no Python 3: + (soma ou concatenação), - (subtração), * (multiplicação), / (quociente da divisão), % (rest da divisãoo) e ** (potência)
-taxa = porcentagem / 100
+# Variável auxiliar que será usada em breve
+precisao = 2
 
-parte = valor * taxa
+# Operadores numericos aceitos no Python 3: + (soma ou concatenação), - (subtração), * (multiplicação), / (quociente da divisão), % (rest da divisãoo) e ** (potência)
+# O método nativo round(numero, casas) é recomendado para evitar erros numéricos de máquina em operações com números decimais (float), retornando outro float
+taxa = round(porcentagem / 100, precisao)
+
+parte = round(valor * taxa, precisao)
 
 # O comanod print(texto) retorna o dado no console
 # É necessário converter todos os valores para str na concatenação (operador + entre dois str)
 print('Parte: ' + str(parte))
 
-aumento = valor + parte
-desconto = valor - parte
+aumento = round(valor + parte, precisao)
+desconto = round(valor - parte, precisao)
 
 print('Aumento: ' + str(aumento))
 print('Desconto: ' + str(desconto))
