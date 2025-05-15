cd709c5f3d31b5d3d3b600844fcfabec0b538c68
Arthur Estigarribia <211487519+arthur-estigarribia-2000@users.noreply.github.com>
2025-05-15 00:08:40 -0300

    Create exemplo003.py
    Este exemplo aborda a noção de estruturas de repetição em Python 3.

---
+++ exemplo003.py
@@ -0,0 +1,20 @@
+# Repetição
+# Exemplo: montagem da tabuada multiplicativa de um inteiro
+
+numero = int(input('Digite um inteiro: '))
+
+# Contador
+i = 0
+
+# Estrutura de repetição while
+# O bloco é executado enquanto a condição for verdadeira, parando apenas quando for falsa
+# Indentação obrigatória
+while i <= 10:
+	resultado = i * numero
+
+	# A concatenação textual exige operandos do tipo str
+	print(str(i) + ' * ' + str(numero) + ' = ' + str(resultado))
+
+	# Para poder alterar o valor de i até que a condição seja falsa, devemos incrementar seu valor
+	# Sem isso, o código fica em loop infinito, não parando devido à condição ser sempre verdadeira
+	i = i + 1
