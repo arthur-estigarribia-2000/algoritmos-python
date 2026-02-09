4e04756a8a314dc99812a97496d05ab3165cc620
Arthur Estigarribia <211487519+arthur-estigarribia-2000@users.noreply.github.com>
2026-02-09 18:58:11 -0300

    Enhance lists and add list copy example
    Added more registers to the list and demonstrated list copying.

--- exemplo005.py
+++ exemplo005.py
@@ -2,13 +2,14 @@
 # Exemplo: registros de bandas
 
 # Declaração de uma lista (array) com tipos homogêneos
-bandas = ['Linkin Park', 'Evanescence', 'Simple Plan', 'Muse']
+bandas = ['Linkin Park', 'Evanescence', 'Simple Plan', 'Muse', 'Avenged Sevenfold', 'Green Day']
 
 # Mostra o elemento na posição inteira indicada (as posições de 0 a n - 1)
 print(str(bandas[1]))
 
 # Acrescenta o elemento ao final da lista
 bandas.append('Spiritbox')
+bandas.append('Bad Omens')
 
 # Mostra todos os valores no console
 print(str(bandas))
@@ -24,3 +25,14 @@ lista.pop(pos - 1)
 lista.pop(1)
 
 print(lista)
+
+# Cópia da lista
+import copy
+
+# A lógica usual lista2 = lista faz referência dinâmica mutável e não cópia, para cópia, usa-se copy.deepcopy(array), que exige a importação (import copy)
+lista2 = copy.deepcopy(lista)
+
+lista2.pop(2)
+
+print('Tamanho da lista inicial: ' + str(len(lista)))
+print('Tamanho da lista 2: ' + str(len(lista2)))
